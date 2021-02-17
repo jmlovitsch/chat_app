0808e06ba9bded666c0840ae42b64827ec71ba12
john <jmlovitsch@gmail.com>
2021-02-17 13:30:05 -0600

    mvp users and rooms available

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,15 +23,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
 
-    respond_to do |format|
+    @message.user = current_user
       if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+        redirect_to request.referrer
       end
-    end
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
