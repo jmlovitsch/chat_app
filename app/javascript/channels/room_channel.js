273088d9d9eae2c47a214faeaee3dd30f79de787
john <jmlovitsch@gmail.com>
2021-02-17 17:43:50 -0600

    refactored html response for room received

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -16,12 +16,27 @@ consumer.subscriptions.create(
       const element = document.getElementById("user_id");
       const user_id = Number(element.getAttribute("data-user-id"));
       const message_user_id = data.message.user_id;
+      let stringToHTML = function (str) {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(str, "text/html");
+        return doc.body;
+      };
+
+      // console.log(stringToHTML(data.html).outerHTML.replace("<body>", "").replace("</body>", ""))
 
       if (user_id == message_user_id) {
-        const newData = data.html.replace("message", "message me");
-        const newerData = newData.replace(data.user.username, "You")
+        const newData = stringToHTML(data.html);
+
+        const classname_node = newData.getElementsByClassName("message");
+
+        classname_node[0].className = "message me";
+        const newerData = newData.getElementsByClassName("author");
+        newerData[0].innerText = "You";
+        const stringed = newData.outerHTML
+          .replace("<body>", "")
+          .replace("</body>", "");
         const messageContainer = document.getElementById("messages");
-        return (messageContainer.innerHTML += newerData);
+        return (messageContainer.innerHTML += stringed);
       } else {
         const messageContainer = document.getElementById("messages");
         return (messageContainer.innerHTML += data.html);
